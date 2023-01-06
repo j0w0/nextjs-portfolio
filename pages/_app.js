82c101a49b7d8f063a35285b6dade6aa8794b22d
Josh Woodcock <josh@j0w0.com>
2023-01-06 02:33:19 -0600

    add google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
 import "../styles/globals.css";
+import { Arimo, Imbue } from "@next/font/google";
 import Layout from "../components/Layout/Layout";
+
+const arimo = Arimo({
+  weight: "400",
+  subsets: "latin",
+});
+
+const imbue = Imbue({
+  weight: "900",
+  subsets: "latin",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-arimo: ${arimo.style.fontFamily};
+          --font-imbue: ${imbue.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
