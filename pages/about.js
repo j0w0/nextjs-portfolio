e0a7a24a30278853598670771a4da0ee1de378b2
Josh Woodcock <josh@j0w0.com>
2023-01-06 00:55:26 -0600

    move layout to _app component

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,10 @@
 import HTMLHead from "../components/HTMLHead/HTMLHead";
-import Layout from "../components/Layout/Layout";
 
 export default function Home() {
   return (
     <>
       <HTMLHead title="About!" />
-      <Layout>
-        <main>About</main>
-      </Layout>
+      <main>About</main>
     </>
   );
 }
