15be8c693a595ed5bebc30f5b36e207d5357b26a
Josh Woodcock <josh@j0w0.com>
2023-01-06 00:57:38 -0600

    add contact page

---
+++ pages/contact.jsx
@@ -0,0 +1,10 @@
+import HTMLHead from "../components/HTMLHead/HTMLHead";
+
+export default function Contact() {
+  return (
+    <>
+      <HTMLHead title="Contact!" />
+      <main>Contact</main>
+    </>
+  );
+}
