d0a3a410f6dddb5be564fd769d7532b135dd430a
Josh Woodcock <josh@j0w0.com>
2023-01-09 01:55:39 -0600

    add placeholder/wip to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <header>j0w0</header>;
+  return (
+    <header>
+      <Link href="/">j0w0</Link>
+    </header>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import HTMLHead from "../components/HTMLHead/HTMLHead";
 
@@ -7,6 +8,14 @@ export default function Home({ page }) {
       <HTMLHead title="Josh Woodcock / Front-End Developer" />
       <main className={styles.main}>
         <h1 className="font-secondary">j0w0</h1>
+        <p>
+          this is currently a wip! im using next.js to build this headless{" "}
+          <Link href="/work">portfolio</Link>. im using{" "}
+          <a href="https://www.j0w0.com/" target="_blank" rel="noreferrer">
+            my current wordpress site
+          </a>{" "}
+          as the backend.
+        </p>
       </main>
     </>
   );
