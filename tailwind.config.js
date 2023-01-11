8e25f86b1d36b9cdac6ad0d6024f4e17e569c1ab
Josh Woodcock <josh@j0w0.com>
2023-01-10 20:50:01 -0600

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx}",
+    "./src/components/**/*.{js,ts,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
