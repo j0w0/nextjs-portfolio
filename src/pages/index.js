488ede3f4a6fd8cfeafb28aa729d0578d0533715
Josh Woodcock <josh@j0w0.com>
2023-01-10 23:53:08 -0600

    add timeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,32 @@ export default function Home({ page }) {
       <main className={styles.main}>
         <h1 className="font-secondary">j0w0</h1>
         <p>
-          this is currently a wip! im using next.js to build this headless{" "}
+          this is a wip. im using next.js to build this headless{" "}
           <Link href="/work">portfolio</Link>. im using{" "}
           <a href="https://www.j0w0.com/" target="_blank" rel="noreferrer">
             my current wordpress site
           </a>{" "}
-          as the backend.
+          as the backend data source.
+        </p>
+        <p>
+          230104 - started with create-next-app
+          <br />
+          230106 - added page structure and google fonts, still need to pick
+          selections
+          <br />
+          230108 - added graphql queries to get wordpress data
+          <br />
+          230109 - set up next/image for featured images
+          <br />
+          230110 - added tailwindcss, not sure if i like it over bootstrap
+          <br />
+        </p>
+        <p>
+          check out my{" "}
+          <a href="https://github.com/j0w00" target="_blank" rel="noreferrer">
+            github
+          </a>{" "}
+          👨🏻‍💻
         </p>
       </main>
     </>
