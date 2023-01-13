25ad9921d2c0eb9fd35c2014d23fe6c5b33a6f92
Josh Woodcock <josh@j0w0.com>
2023-01-13 00:16:41 -0600

    add images to single project

--- src/pages/work/[slug].jsx
+++ src/pages/work/[slug].jsx
@@ -4,9 +4,20 @@ import {
   getInteractivePortfolioPosts,
   getPortfolioPost,
 } from "../../lib/wp-graphql";
+import Image from "next/image";
 
 export default function Post({ post }) {
-  const { projectId, slug, title, content, websiteUrl, projectTags } = post;
+  const {
+    attachedMedia,
+    content,
+    databaseId,
+    featuredImage,
+    projectTags,
+    slug,
+    title,
+    websiteUrl,
+  } = post;
+
   return (
     <>
       <HTMLHead title={title} />
@@ -14,17 +25,28 @@ export default function Post({ post }) {
         <h1 className="font-secondary">{title}</h1>
         <div dangerouslySetInnerHTML={{ __html: content }} />
 
-        <ul className="list-none p-0 flex items-center flex-wrap gap-2">
+        <ul className="list-none p-0 mb-4 flex items-center flex-wrap gap-2">
           {projectTags.edges.map((tag) => {
-            const key = tag.node.id;
-            const name = tag.node.name;
             return (
-              <li key={key} className="px-2 py-1 bg-zinc-100">
-                {name}
+              <li key={tag.node.id} className="px-2 py-1 bg-zinc-100">
+                {tag.node.name}
               </li>
             );
           })}
         </ul>
+
+        {attachedMedia.edges.map((image) => {
+          image = image.node;
+          return (
+            <Image
+              key={image.databaseId}
+              src={image.mediaItemUrl}
+              height={image.mediaDetails.height}
+              width={image.mediaDetails.width}
+              alt={image.altText}
+            />
+          );
+        })}
       </main>
     </>
   );
