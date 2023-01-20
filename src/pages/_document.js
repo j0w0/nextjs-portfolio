978937025e0051f7684e47c8f00cd657d2109586
Josh Woodcock <josh@j0w0.com>
2023-01-20 16:13:43 -0600

    add meta description

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const description = "Josh Woodcock, Front-End Developer & Software Engineer";
+
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="Josh Woodcock, Front-End Developer & Software Engineer"
-        />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="/j0w0-meta-img.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
