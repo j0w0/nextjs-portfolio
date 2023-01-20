2686cb9599074d52721759a2f3475874dc1505da
Josh Woodcock <josh@j0w0.com>
2023-01-20 16:24:11 -0600

    og meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const title = "Josh Woodcock // Front-End Developer";
   const description =
     "Front-End Developer and Software Engineer experienced in building high-quality, manageable, and scalable websites and web applications.";
 
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content="/j0w0-meta-img.jpg" />
+        <meta name="title" property="og:title" content={title} />
+        <meta
+          name="description"
+          property="og:description"
+          content={description}
+        />
+        <meta name="image" property="og:image" content="/j0w0-meta-img.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
