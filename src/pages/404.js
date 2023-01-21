74fee4f7d19fc8ce86d44272670076b47812e2a6
Josh Woodcock <josh@j0w0.com>
2023-01-21 02:44:48 -0600

    custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import HTMLHead from "../components/HTMLHead/HTMLHead";
+import PageHeading from "../components/PageHeading/PageHeading";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+export default function Custom404() {
+  const title = "404!";
+  return (
+    <>
+      <HTMLHead title={title} />
+      <PageHeading title="I- uh..." />
+      <section className="flex flex-col">
+        <div className="container py-10">
+          <div
+            className="
+            grid
+            lg:grid-cols-4
+            lg:gap-6
+            xl:gap-8
+            "
+          >
+            <div className="lg:col-span-3">
+              <h2>Couldnt find what you were looking for.</h2>
+              <p>
+                <Link href="/">Go home</Link> or check out the links{" "}
+                <span className="lg:hidden">below 👇🏼</span>
+                <span className="hidden lg:inline-block">over there 👉🏼</span>
+              </p>
+            </div>
+            <Sidebar />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
